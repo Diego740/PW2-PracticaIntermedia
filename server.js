833fa41c5d8264d915a65cfb695ea5c8f02341a1
Diego740 <57465270+Diego740@users.noreply.github.com>
2025-03-12 11:39:24 +0100

    DB connect

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+require ('dotenv').config()
+
+//const routers = require('./routes')
+
+const dbConnect = require('./config/mongo.js')
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`)
+})
+
+dbConnect()
