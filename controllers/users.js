22606532b6750846c1759bbea84b3a59fe4996a6
Diego740 <57465270+Diego740@users.noreply.github.com>
2025-03-27 02:02:59 +0100

    register fin

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 const UserModel = require("../models/users.js");
 const { matchedData } = require("express-validator");
 const { handleHttpError } = require("../utils/handleError.js");
+const { encryptPassword } = require("../utils/handlePassword.js");
+const jwt = require("jsonwebtoken");
 
 
 const createItem = async (req, res) => {
@@ -10,15 +12,40 @@ const createItem = async (req, res) => {
         //Verificar email registrado
         const existingUser = await UserModel.findOne({ email: body.email });
         if (existingUser) {
-            return res
-                .status(409)
-                .json({ message: "El email ya está registrado" });
+            return res.status(409).json({ message: "El email ya está en uso" });
         }
 
-        const data = await UserModel.create(body);
-        res.send(data);
+        //Cifrar la contraseña
+        const hashedPassword = await encryptPassword(body.password);
+
+        //Generar código de verificación de 6 dígitos
+        const verificationCode = Math.floor(100000 + Math.random() * 900000);
+        
+        //Crear usuario
+        const newUser = await UserModel.create({
+            email: body.email,
+            password: hashedPassword,
+            //role: "user",
+            //attemps: 3,
+            code: verificationCode,
+            //verificated: false
+        });
+        
+        //const newUser = await UserModel.create(body);
+
+        //Generar token JWT
+        const token = jwt.sign({ id: newUser._id }, process.env.TOKEN_SECRET, { expiresIn: "1h" });
+
+        console.log(verificationCode);
+        return res.status(201).json({
+            email: newUser.email,
+            verificated: newUser.verificated,
+            role: newUser.role,
+            token,
+        });
     } catch (err) {
-        handleHttpError(res, "ERROR_CREATE_ITEMS");
+        //console.error(err);
+        handleHttpError(res, "ERROR_CREATE_ITEMS", 500);
     }
 };
 
---
+++ utils/handlePassword.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt");
+
+
+const encryptPassword = async (password) => {
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    return hashedPassword;
+
+}
+
+module.exports = { encryptPassword };
