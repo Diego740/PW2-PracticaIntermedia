db583da4d9de392b7697daee362e7233bbf2da68
Diego740 <57465270+Diego740@users.noreply.github.com>
2025-03-31 15:00:55 +0200

    fixMail

--- validators/mail.js
+++ validators/mail.js
@@ -1,14 +1,12 @@
 const { check } = require("express-validator");
-const { validateResults } = require("../utils/handleEmail.js");
+const validateResults = require("../utils/handleValidator");
 
 const validatorMail = [
   check("subject").exists().notEmpty(),
   check("text").exists().notEmpty(),
   check("to").exists().notEmpty(),
   check("from").exists().notEmpty(),
-  (req, res, next) => {
-    return validateResults(req, res, next);
-  },
+  validateResults
 ];
 
 module.exports = { validatorMail };
